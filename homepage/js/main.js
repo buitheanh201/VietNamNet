1d8b455120a416178620bc495fb380ee0917308c
Bui The Anh <bupbui95@gmail.com>
2021-04-12 01:05:48 +0700

    push new data

--- homepage/js/main.js
+++ homepage/js/main.js
@@ -1,8 +1,9 @@
 window.addEventListener("DOMContentLoaded",(e) => {
    const navigation = document.querySelector('.navigation');
+   const sticky = navigation.offsetTop;
    window.addEventListener('scroll',() => {
         const windowPageYOffset = window.pageYOffset;
-        const sticky = navigation.offsetTop;
+        console.log(windowPageYOffset)
         if(windowPageYOffset > sticky) navigation.classList.add('sticky__nav')
         else navigation.classList.remove('sticky__nav')
    })
