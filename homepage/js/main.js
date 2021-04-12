3fa334599785e27dc12f076ff0b999df9d6e925d
Bùi Thế Anh <73353082+buitheanh201@users.noreply.github.com>
2021-04-12 11:24:42 +0700

    push new content

--- homepage/js/main.js
+++ homepage/js/main.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded",(e) => {
    const sticky = navigation.offsetTop;
    window.addEventListener('scroll',() => {
         const windowPageYOffset = window.pageYOffset;
-        console.log(windowPageYOffset)
+      //   console.log(windowPageYOffset)
         if(windowPageYOffset > sticky) navigation.classList.add('sticky__nav')
         else navigation.classList.remove('sticky__nav')
    })
